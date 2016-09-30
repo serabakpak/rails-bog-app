a066477345bebdfe6902fc3b1e647b984ab86ed0
Sera Bak <serabak15@gmail.com>
2016-09-30 15:56:29 -0700

    creates form for new and create actions

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -1,6 +1,25 @@
 class CreaturesController < ApplicationController
 
 	def index
+		#get all the creatures from db and save to instance variable
+		#don't need render :index because this is default behavior
 		@creatures = Creature.all
+	end
+
+	def new
+		#don't need render :new because this is default
+		@creature = Creature.new
+	end
+
+	def create
+		creature_params = params.require(:creature).permit(:name, :description)
+
+		creature = Creature.new(creature_params)
+
+		if creature.save
+			redirect_to creatures_path
+		end
+
+
 	end
 end
