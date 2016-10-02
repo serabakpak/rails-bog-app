e757da57762b49375f9042d4eaa840a2489cb458
Sera Bak <serabak15@gmail.com>
2016-10-01 23:57:04 -0700

    adds edit form

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -10,14 +10,23 @@ class CreaturesController < ApplicationController
 
 	def show
 		@creature = Creature.find_by_id(params[:id])
-		
+
+	end
+
+	def edit
+		@creature = Creature.find_by_id(params[:id])
 	end
 
 	def create
 		creature = Creature.create(creature_params)
-		redirect_to creatures_path
+		redirect_to creature_path(creature)
 	end
 
+	def update
+		creature = Creature.find_by_id(params[:id])
+		creature.update_attributes(creature_params)
+		redirect_to creatures_path
+	end
 
 	private
 		def creature_params
