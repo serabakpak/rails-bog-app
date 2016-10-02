86199336b6f41fa9e430ea8038adb2e89af4698c
Sera Bak <serabak15@gmail.com>
2016-10-01 22:04:08 -0700

    creates form and show page

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -4,7 +4,25 @@ class CreaturesController < ApplicationController
 		@creatures = Creature.all
 	end
 
+	def new
+		@creature = Creature.new
+	end
 
+	def show
+		creature_id = params[:id]
+		@creature = Creature.find_by_id(creature_id)
+	end
 
+	def create
+		creature = Creature.new(creature_params)
+		if creature.save
+			redirect_to creature_path(creature)
+		end
+	end
+
+	private
+		def creature_params
+			params.require(:creature).permit(:name, :description)
+		end
 
 end
